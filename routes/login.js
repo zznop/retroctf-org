a15f9ec10d6efe6641a6865750cd131efdc087df
zznop <zznop0x90@gmail.com>
2019-08-17 23:21:58 -0400

    Added a simple header to the main layout (mainly for logout link)

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ const authUtils = require('../auth-utils');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Retro CTF', status: req.query.status });
+  res.render('login', { title: 'Retro CTF', status: req.query.status});
 });
 
 router.post('/', async function(req, res, next) {
